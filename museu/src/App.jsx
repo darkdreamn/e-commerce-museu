bd25cd0ca85cb86ce8078a024a5995766db8ef5c
RafaelM <dark-dreamn@hotmail.com>
2022-02-10 23:55:32 -0300

    criação do título

--- museu/src/App.jsx
+++ museu/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import './reset.css'
 import Header from './components/header/Header'
 import Gallery from './components/gallery/Gallery'
+import Title from './components/title/title'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Gallery />
+      <Title />
     </div>
   )
 }
